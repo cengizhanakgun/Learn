212f4852d4223e92679177d170fe543ec01a7c6b
cengizhanakgun <coderista@gmail.com>
2023-05-12 16:33:33 +0300

    Add project files.

---
+++ Learn/Form1.cs
@@ -0,0 +1,91 @@
+﻿using Learn.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Learn
+{ //Begin
+    //Scope: Bir kod bloğunun nerede başladığı ve nerede sonlandığını belirten yapılardır.
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+
+            //Tek satır açıklama 
+
+            /*Çok satırlı açıklama örneği
+             * 
+             * 
+             */
+
+            /*
+             * 
+             * 
+             */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            #region Açıklama
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.   
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır.
+            //Yorum Satır. 
+            #endregion
+
+            //Region: CTRL+K+S > C# >Region
+
+            //F7 FORM DEGSİGN AÇIKKEN AKTİF FORM'UN KOD DOSYASI AÇILIR.
+            //F6 DERLE AMA ÇALIŞTIRMA.
+            //F5 DERLE ÇALIŞTIR.
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //new Form3().ShowDialog();
+            //new Form3().ShowDialog(); //CTRL+D İLE AYNI KOD SATIRINI ALT SATIRA KOPYALARSIN.
+            //new Form3().ShowDialog();
+
+         
+
+        }
+    }
+}//End
+
+
+
